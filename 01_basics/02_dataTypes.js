9c28160743f7bc47ea4eb9206f962864bf09bb6b
kishorlangote <115852670+kishorlangote@users.noreply.github.com>
2023-10-08 14:39:34 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+// "use strict"; // treat all JS code as newer version.
+// alert (3 + 3) -- we are using nodejs, not browser. 
+
+// console.log(3
+//      + 
+//     // 3)  code readability should be high
+
+console.log("Kishor")
+
+let name = "Kishor"
+let age = 18
+let isLoggedIn = false 
+
+/* data types : 
+
+number 
+String = ""
+boolean = true / false
+bigint
+null = standalone value
+undefined
+symbol = unique
+object = 
+
+
+*/
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);       // object 
+// assingment for you - visit the ecma javascript official website and see specification 
