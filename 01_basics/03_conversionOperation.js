27404848e33a45a04e21c488dae50c0dd7c1cde5
kishorlangote <115852670+kishorlangote@users.noreply.github.com>
2023-10-08 15:22:01 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+// let score = "33abd"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" = 33 string to number
+// "33abd" = NaN (Not an number, but this is number data types)
+// true = 1; false = 0;
+
+// let isLoggedIn = ""
+// let BooleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanisLoggedIn);
+
+// 1 = true; 0 = false; 
+// "" = false
+// "kishor" = true
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
