1fb818fe2a30028f15031d8b3608f0e3b6b39055
kishorlangote <115852670+kishorlangote@users.noreply.github.com>
2023-10-09 09:12:58 +0000

    dataypes done

---
+++ 01_basics/dataType-Summary.js
@@ -0,0 +1,25 @@
+// Primitve data types : 
+// 7 types : String, Number, Boolean, undefined, symbole(make a unique), BigInt(use in scientific value), null.
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 2546874458875542455568855n
+
+// Non-primitive/Reference data types :
+//3 types : Array, objects, functions.
+
+const heros = ["shaktiman", "john", "maharaj"]; 
+
+// const myObj = {
+//     name: "kishor",
+//     age: 18,
+// }
+
+// const myfunction = function(){
+//     console.log("Hello World");
+// }
+
+console.log(typeof heros);
