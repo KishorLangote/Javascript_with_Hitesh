5b83c6d588f95fd47ec22d389bac63bc47b95e5a
kishorlangote <115852670+kishorlangote@users.noreply.github.com>
2023-10-09 15:59:21 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,21 @@
+//  Array - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+const myArr = [1, 3, 2, 5, 8]
+const myHeros = ["bharadwaj", "devy", "rebert"]
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[6]);
+
+// Array methods 
+
+// myArr.push(6)
+// myArr.push(9)
+// myArr.pop()
+
+// myArr.unshift(7)
+// myArr.shift()
+
+//console.log(myArr);
+
+const newArr = myArr.join()
+console.log(myArr);
+console.log(newArr);
