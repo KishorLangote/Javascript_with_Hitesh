6484c1f538672d71750c883e0ec42aee5b841153
kishorlangote <115852670+kishorlangote@users.noreply.github.com>
2023-10-25 04:34:52 +0000

    iteration

---
+++ 05_iterations/one.js
@@ -0,0 +1,10 @@
+// for
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element ==5 ) {
+        console.log("5 is best number");
+    }
+    console.log(element);
+    
+}
+// console.log(element);
